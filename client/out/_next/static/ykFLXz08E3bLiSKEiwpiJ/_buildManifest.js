f38e08260fd89b6f5baf12bda0c50f17133a08c0
Martin Edwards <martin.edwards@creative-assembly.com>
2021-07-13 17:07:21 +0100

    Don't commit export or .next folder

--- client/out/_next/static/ykFLXz08E3bLiSKEiwpiJ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-a0c902a33ad806be2504.js"],"/_error":["static/chunks/pages/_error-f69444425b2d6c4d374c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
