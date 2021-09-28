e2ec23c32b814946f76c5b39152d8345bb583066
Martin Edwards <martin@spin-up.io>
2021-09-28 19:50:15 +0100

    Moved callback logic so it's inside the Provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,13 @@
 // import App from 'next/app'
 import React from "react";
-import { App as CapApp } from "@capacitor/app";
-import { Browser } from "@capacitor/browser";
-import { useAuth0 } from "@auth0/auth0-react";
-import { Auth0Provider } from "@auth0/auth0-react";
+import { Auth0Provider } from '@auth0/auth0-react';
 
 function MyApp({ Component, pageProps }) {
-  const { handleRedirectCallback } = useAuth0();
-
-  React.useEffect(() => {
-    // Handle the 'appUrlOpen' event and call `handleRedirectCallback`
-    CapApp.addListener("appUrlOpen", async ({ url }) => {
-      if (
-        url.includes("state") &&
-        (url.includes("code") || url.includes("error"))
-      ) {
-        await handleRedirectCallback(url);
-      }
-      // No-op on Android
-      await Browser.close();
-    });
-  }, [handleRedirectCallback]);
-
   return (
     <Auth0Provider
       domain="mymotorhub.eu.auth0.com"
       clientId="OIJgUdKZhRgFeMcKzxCNdL19vpiiRAEa"
-      redirectUri="app.vercel.mymotorhub://mymotorhub.eu.auth0.com/capacitor/app.vercel.mymotorhub/callback"
+      redirectUri="com.mymotorhub.app://mymotorhub.eu.auth0.com/capacitor/com.mymotorhub.app/callback"
     >
       <Component {...pageProps} />
     </Auth0Provider>
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,37 @@
 import React from "react";
+import { App as CapApp } from '@capacitor/app';
+import { Browser } from '@capacitor/browser';
+
 import { useAuth0 } from "@auth0/auth0-react";
 import LoginButton from "../components/Login";
 import LogoutButton from "../components/Logout";
 import Profile from "../components/Profile";
 
 export default function Index() {
-  const { isAuthenticated } = useAuth0();
+  // Get the callback handler from the Auth0 React hook
+  const { isAuthenticated, handleRedirectCallback } = useAuth0();
+
+  React.useEffect(() => {
+    // Handle the 'appUrlOpen' event and call `handleRedirectCallback`
+    CapApp.addListener("appUrlOpen", async ({ url }) => {
+      if (
+        url.includes("state") &&
+        (url.includes("code") || url.includes("error"))
+      ) {
+        await handleRedirectCallback(url);
+      }
+      // No-op on Android
+      await Browser.close();
+    });
+  }, [handleRedirectCallback]);
 
   return (
     <div>
       <div className="container">
         <Profile />
-        {isAuthenticated ? <LogoutButton /> : <LoginButton />}
+        <LogoutButton />
+        <LoginButton />
+        
       </div>
     </div>
   );
